1f1aa0c2e5c22b0b7f6be230f75793721302c790
token <239573049@qq.com>
2023-07-26 02:51:44 +0800

    异常

--- src/Chat.Service/Services/AuthService.cs
+++ src/Chat.Service/Services/AuthService.cs
@@ -70,42 +70,50 @@ public class AuthService : BaseService<AuthService>
     public async Task<ResultDto<string>> GiteeAuthAsync(string accessToken,
         string redirect_uri = "http://localhost:3000/login?type=gitee")
     {
-        using var http = new HttpClient();
-        var gitee = GetOptions<GiteeOptions>();
-        var response =
-            await http.PostAsync(
-                $"https://gitee.com/oauth/token?grant_type=authorization_code&redirect_uri=http://124.222.89.53/?type=gitee&response_type=code&code={accessToken}&client_id={gitee.ClientId}&client_secret={gitee.ClientSecrets}",
-                null);
-        var result = await response.Content.ReadFromJsonAsync<GitTokenDto>();
-        if (result is null) throw new Exception("Gitee授权失败");
-
-        var githubUser =
-            await http.GetFromJsonAsync<GiteeDto>("https://gitee.com/api/v5/user?access_token=" + result.access_token);
-        if (githubUser is null) throw new Exception("Gitee授权失败");
-
-        var query = new AuthTypeQuery("Gitee", githubUser.id.ToString());
-        await _eventBus.PublishAsync(query);
-
-        if (query.Result is null)
+        try
         {
-            var command = new CreateUserCommand(new CreateUserDto
+            using var http = new HttpClient();
+            var gitee = GetOptions<GiteeOptions>();
+            var response =
+                await http.PostAsync(
+                    $"https://gitee.com/oauth/token?grant_type=authorization_code&redirect_uri=http://124.222.89.53/?type=gitee&response_type=code&code={accessToken}&client_id={gitee.ClientId}&client_secret={gitee.ClientSecrets}",
+                    null);
+
+            var result = await response.Content.ReadFromJsonAsync<GitTokenDto>();
+            if (result is null) throw new Exception("Gitee授权失败");
+
+            var githubUser =
+                await http.GetFromJsonAsync<GiteeDto>("https://gitee.com/api/v5/user?access_token=" + result.access_token);
+            if (githubUser is null) throw new Exception("Gitee授权失败");
+
+            var query = new AuthTypeQuery("Gitee", githubUser.id.ToString());
+            await _eventBus.PublishAsync(query);
+
+            if (query.Result is null)
             {
-                Account = "guest_" + StringHelper.RandomString(8),
-                Avatar = githubUser.avatar_url,
-                Name = githubUser.name,
-                Password = "Aa123456",
-                GiteeId = githubUser.id.ToString()
-            });
+                var command = new CreateUserCommand(new CreateUserDto
+                {
+                    Account = "guest_" + StringHelper.RandomString(8),
+                    Avatar = githubUser.avatar_url,
+                    Name = githubUser.name,
+                    Password = "Aa123456",
+                    GiteeId = githubUser.id.ToString()
+                });
 
-            await _eventBus.PublishAsync(command);
-            query.Result = command.Result;
+                await _eventBus.PublishAsync(command);
+                query.Result = command.Result;
+            }
+
+            var claims = JwtHelper.GetClaimsIdentity(query.Result);
+            var jwt = GetService<IOptions<JwtOptions>>()?.Value;
+
+            var token = JwtHelper.GeneratorAccessToken(claims, jwt);
+
+            return token.CreateResult();
         }
-
-        var claims = JwtHelper.GetClaimsIdentity(query.Result);
-        var jwt = GetService<IOptions<JwtOptions>>()?.Value;
-
-        var token = JwtHelper.GeneratorAccessToken(claims, jwt);
-
-        return token.CreateResult();
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
