7d1f16217ad321fb8ec9fbe3a53ce331413ec5b5
token <239573049@qq.com>
2023-07-26 14:03:25 +0800

    优化重新连接

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -14,24 +14,39 @@ import config from './config';
 dayjs.locale('zh-cn');
 
 const connection = new signalR.HubConnectionBuilder()
-    .withUrl(config.API_URL+"/chatHub", { accessTokenFactory: () => localStorage.getItem('token') })
-    .withHubProtocol(new MessagePackHubProtocol())
-    .configureLogging(signalR.LogLevel.Information)
-    .build();
+  .withUrl(config.API_URL + "/chatHub", { accessTokenFactory: () => localStorage.getItem('token') })
+  .withHubProtocol(new MessagePackHubProtocol())
+  .configureLogging(signalR.LogLevel.Information)
+  .build();
 
 window.connection = connection;
 
 
 await connection.start();
 
-connection.on("UpdateOnline",(i)=>{
+connection.on("UpdateOnline", (i) => {
   console.log('更新在线', i);
   // 发布事件
   PubSub.publish('UpdateOnline', i);
 })
 
+let i = 0;
+
 connection.onclose(async () => {
-  message.error('连接已断开，正在重连...');
+  message.error('连接已断开，正在重连...', 5);
+  i++;
+  if (i === 10) {
+    return;
+  }
+  await connection.start()
+    .then(() => {
+      // 重新连接成功
+      message.success('重连成功', 3);
+    })
+    .catch(x => {
+      // 重新连接失败
+      message.error('重连失败，请刷新页面', 5);
+    });
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
