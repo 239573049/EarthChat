a5f829ad45f26490af3e358b5004804cefa1b756
token <239573049@qq.com>
2023-10-25 14:13:27 +0800

    修改编辑页面

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -68,14 +68,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://gitee.com/hejiale010426/chat/tree/master/docs/docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -166,7 +159,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['powershell','csharp','javascript','json'],
+        additionalLanguages: ['powershell', 'csharp', 'javascript', 'json'],
       },
     }),
 };
